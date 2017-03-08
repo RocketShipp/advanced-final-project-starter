5e388df93ad95d9f0516addea4d1a3eb364e3b91
Austin Shipp <austinshipp@yahoo.com>
2017-03-08 17:48:09 -0600

    initial commit

--- src/routes/AuthenticationRoutes.js
+++ src/routes/AuthenticationRoutes.js
@@ -2,4 +2,57 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jwt-simple');
 const User = require('../models/UserModel');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
+const passport = require('passport');
+
+require('../services/passport');
+
+const signInStrategy = passport.authenticate('signInStrategy', { session: false });
+
+// Creates token for the user
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode({ userId: user.id, iat: timestamp}, process.env.SECRET);
+}
+
+router.post('/api/signin', signInStrategy, function (req, res, next) {
+  res.json({ token: tokenForUser(req.user)});
+});
+
+router.post('/api/signup', function (req, res, next) {
+  const { username, password } = req.body;
+
+  // No username or password supplied
+  if ( !username || !password) {
+    return res.status(422).json({ error: 'You must provide a username and password!'});
+  }
+
+  // Look for a user with the current username
+  User.findOne({ username }).exec()
+    .then((existingUser) => {
+      if (existingUser) {
+        return res.status(422).json({ error: 'Username has already been taken'});
+      }
+
+      // If the username doesnt exist yet, create the user and bcrypt the password
+      bcrypt.genSalt(10, function (salt) {
+        bcrypt.hash(password, salt, null, function (err, hashedPassword) {
+          if (err) {
+            return next(err);
+          }
+
+          // Create new user with supplied username and hashed password
+          const newUser = new User({ username, password: hashedPassword});
+
+          // Save and return user
+          newUser.save().then(user => res.json({ token: tokenForUser(user)}));
+        });
+      });
+    })
+    .catch(err => {
+      next(err);
+    });
+
+});
+
+module.exports = router;
--- src/services/passport.js
+++ src/services/passport.js
@@ -1,5 +1,52 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
 const passport = require('passport');
 const User = require('../models/UserModel');
 const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const LocalStrategy = require('passport-local');
+
+const signinStrategy = new LocalStrategy(function (username, password, done) {
+  User.findOne({ username: username }).exec()
+    .then(user => {
+      // If no user, call done with NULL argument and false signifying error
+      if (!user) {
+        return done(null, false);
+      }
+
+      bcrypt.compare(password, user.password, function (err, isMatch) {
+        if (err) {
+          return done(err, false);
+        }
+        // If password does not match call done with NULL and false
+        if (!isMatch) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+    })
+    .catch(err => done(err, false));
+});
+
+// JwtStrategy set-up
+const jwtOptions = {
+  // Get secret from env file
+  secretOrKey: process.env.SECRET,
+  // Location of token in request header
+  jwtFromRequest: ExtractJwt.fromHeader('authorization')
+};
+
+// Create strategy that accepts token and decodes it
+const authStrategy = new JwtStrategy(jwtOptions, function (payload, done) {
+  User.findById(payload.userId, function (err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+
+// Tell passport to use this strategy
+passport.use('authStrategy', authStrategy);
+passport.use('signinStrategy', signinStrategy);
